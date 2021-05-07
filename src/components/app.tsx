60d446b0a8e43b8f52a977c2c707081d82967550
lexie-kaia <kaia.gayeon.kim@gmail.com>
2021-05-07 11:04:49 +0900

    add fontawesome and initial styling

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from 'components/app.module.css';
 import Header from 'components/header/header';
-import Basket from 'components/basket';
-import Footer from 'components/footer';
+import Basket from 'components/basket/basket';
+import Footer from 'components/footer/footer';
 
 function App() {
   return (
-    <div>
+    <div className={styles.container}>
       <Header />
       <Basket />
       <Footer />
